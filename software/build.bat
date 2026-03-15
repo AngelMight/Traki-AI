0ed33bfb9053f5ca1e87edf6ecb605e8db0d887a
stefanyovev <stefanyovev@mail.bg>
2026-03-15 13:49:40 +0200

    update software/build.bat

--- software/build.bat
+++ software/build.bat
@@ -1,7 +1,16 @@
 
+
+if not exist "JUCE\extras\Projucer\Builds\VisualStudio2022\x64\Release\App\Projucer.exe" (
+	msbuild JUCE\extras\Projucer\Builds\VisualStudio2022\Projucer.sln /p:Configuration=Release /p:Platform=x64 /m
+)
+
+
 rmdir /s /q Builds
+rmdir /s /q JuceLibraryCode
+
 
 JUCE\extras\Projucer\Builds\VisualStudio2022\x64\Release\App\Projucer.exe --resave main.jucer --ignore-global-settings
+
 msbuild Builds\VisualStudio2022\AudioSettingsDemo.sln /p:Configuration=Release /p:Platform=x64 /t:Rebuild /m
 
 Builds\VisualStudio2022\x64\Release\App\AudioSettingsDemo.exe
